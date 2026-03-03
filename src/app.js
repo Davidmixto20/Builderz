805ea7e599b77259f32fc4f52dd47924fc32d89b
Davidmixto20 <davidaniel0508@gmail.com>
2026-03-03 08:45:12 -0400

    subiendo los archivos que tenian algunos errores, arreglados

--- src/app.js
+++ src/app.js
@@ -4,9 +4,34 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "views")));
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.get("/blog", (req, res) => {
+    res.render("blog");
+})
+app.get("/team", (req, res) => {
+    res.render("team");
+})
+app.get("/single", (req, res) => {
+    res.render("single");
+})
+app.get("/contact", (req, res) => {
+    res.render("contact");
+})
+app.get("/service", (req, res) => {
+    res.render("service");
+})
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+app.get("/portfolio", (req, res) => {
+    res.render("portfolio");
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
