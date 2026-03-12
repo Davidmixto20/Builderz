3b6f401911007bb3bf3835d2e51b68c19f5eaf17
Davidmixto20 <davidaniel0508@gmail.com>
2026-03-12 08:16:05 -0400

    Agregando las nuevas rutas y demas carpetas nuevas con sus archivos

--- src/app.js
+++ src/app.js
@@ -4,35 +4,71 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const services = [
+    {
+        title: "Building Construction",
+        description: "Expert construction services with modern equipment.",
+        image: "/img/service-1.jpg"
+    },
+    {
+        title: "House Renovation",
+        description: "Renovating houses with professional quality.",
+        image: "/img/service-2.jpg"
+    },
+    {
+        title: "Architecture Design",
+        description: "Creative architectural designs for modern homes.",
+        image: "/img/service-3.jpg"
+    },
+    {
+    title: "Interior Design",
+    description: "Modern interior designs.",
+    image: "/img/service-4.jpg"
+    },
+    {
+    title: "Interior Design",
+    description: "Modern interior designs.",
+    image: "/img/service-4.jpg"
+    }
+];
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../public")));
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.render("index");
-})
+    res.render("pages/index", { title: "Home", services: services });
+});
+
 app.get("/blog", (req, res) => {
-    res.render("blog");
-})
+    res.render("pages/blog", { title: "Blog" });
+});
+
 app.get("/team", (req, res) => {
-    res.render("team");
-})
+    res.render("pages/team", { title: "Team" });
+});
+
 app.get("/single", (req, res) => {
-    res.render("single");
-})
+    res.render("pages/single", { title: "Single Page" });
+});
+
 app.get("/contact", (req, res) => {
-    res.render("contact");
-})
+    res.render("pages/contact", { title: "Contact" });
+});
+
 app.get("/service", (req, res) => {
-    res.render("service");
-})
+    res.render("pages/service", { title: "Services" });
+});
+
 app.get("/about", (req, res) => {
-    res.render("about");
-})
+    res.render("pages/about", { title: "About" });
+});
+
 app.get("/portfolio", (req, res) => {
-    res.render("portfolio");
-})
+    res.render("pages/portfolio", { title: "Portfolio" });
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
